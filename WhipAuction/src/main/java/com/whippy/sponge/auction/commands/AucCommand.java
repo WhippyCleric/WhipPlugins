97b51bf7627533de3fffda7ed6df6278655a9d25
Matthew Dunn <mdunn@murex.com>
2015-05-25 12:22:50 +0200

    Remove accidental copyright

--- WhipAuction/src/main/java/com/whippy/sponge/auction/commands/AucCommand.java
+++ WhipAuction/src/main/java/com/whippy/sponge/auction/commands/AucCommand.java
@@ -1,15 +1,7 @@
-/**
- *  Copyright Murex S.A.S., 2003-2015. All Rights Reserved.
- * 
- *  This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
- *  express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
- */
 package com.whippy.sponge.auction.commands;
 
 import java.util.Collections;
 import java.util.List;
-
-import com.google.common.base.Optional;
 
 import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.text.Text;
@@ -19,6 +11,8 @@ import org.spongepowered.api.util.command.CommandCallable;
 import org.spongepowered.api.util.command.CommandException;
 import org.spongepowered.api.util.command.CommandResult;
 import org.spongepowered.api.util.command.CommandSource;
+
+import com.google.common.base.Optional;
 
 
 public class AucCommand implements CommandCallable {
--- WhipAuction/src/main/java/com/whippy/sponge/auction/main/WhipAuction.java
+++ WhipAuction/src/main/java/com/whippy/sponge/auction/main/WhipAuction.java
@@ -1,26 +1,18 @@
-/**
- *  Copyright Murex S.A.S., 2003-2015. All Rights Reserved.
- * 
- *  This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
- *  express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
- */
+
 package com.whippy.sponge.auction.main;
 
 import java.io.IOException;
 
-import com.google.inject.Inject;
-
-import com.whippy.sponge.auction.commands.AucCommand;
-
 import org.json.simple.parser.ParseException;
-
 import org.slf4j.Logger;
-
 import org.spongepowered.api.Game;
 import org.spongepowered.api.event.Subscribe;
 import org.spongepowered.api.event.state.ServerStartingEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.service.command.CommandService;
+
+import com.google.inject.Inject;
+import com.whippy.sponge.auction.commands.AucCommand;
 
 
 @Plugin(id = "WhipAuction", name = "WhipAuction")
