04c2d0576cc44455ca47fd3972d0f0a002b429b2
Matthew Dunn <mdunn@murex.com>
2015-06-29 17:40:12 +0200

    Add savings commands

---
+++ WhipEconomy/src/main/java/com/whippy/sponge/whipconomy/commands/SavingsHistoryCommand.java
@@ -0,0 +1,18 @@
+package com.whippy.sponge.whipconomy.commands;
+
+import org.spongepowered.api.util.command.CommandException;
+import org.spongepowered.api.util.command.CommandResult;
+import org.spongepowered.api.util.command.CommandSource;
+import org.spongepowered.api.util.command.args.CommandContext;
+import org.spongepowered.api.util.command.spec.CommandExecutor;
+
+public class SavingsHistoryCommand implements CommandExecutor{
+
+	@Override
+	public CommandResult execute(CommandSource arg0, CommandContext arg1)
+			throws CommandException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
